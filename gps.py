b33b1798ed1eb00e6369ad9615fbdf1b3a6e76a7
Ari-Berkowicz <berkowicz.ari@gmail.com>
2022-05-04 16:33:15 -0400

    Potentially fixed the code

--- gps.py
+++ gps.py
@@ -5,14 +5,12 @@ import gps
 import time
 import serial
 import sys
-import os
-import datetime
-import math
+import gpsd
 
 
 # open serial port
 ser = serial.Serial(
-    port='/dev/ttyUSB0',
+    port='/dev/serial0',
     baudrate=9600,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
@@ -25,79 +23,31 @@ ser = serial.Serial(
 session = gps.gps("localhost", "2947")
 session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 
+def getPositionData ():
+    nx = gpsd.next()
+    if nx['class'] == 'TPV':
+        latitude = getattr(nx, 'lat', "Unknown")
+        longitude = getattr(nx, 'lon', "Unknown")
+        print("Latitude: %s and Longitude: %s" % latitude, longitude)
 
-# get data from gpsd
+
 while True:
     try:
-        report = session.next()
-        if report['class'] == 'TPV':
-            if hasattr(report, 'time'):
-                print(report.time)
-            if hasattr(report, 'lat'):
-                print(report.lat)
-            if hasattr(report, 'lon'):
-                print(report.lon)
-            if hasattr(report, 'alt'):
-                print(report.alt)
-            if hasattr(report, 'speed'):
-                print(report.speed)
-            if hasattr(report, 'track'):
-                print(report.track)
-            if hasattr(report, 'climb'):
-                print(report.climb)
-            if hasattr(report, 'epx'):
-                print(report.epx)
-            if hasattr(report, 'epy'):
-                print(report.epy)
-            if hasattr(report, 'epv'):
-                print(report.epv)
-            if hasattr(report, 'ept'):
-                print(report.ept)
-            if hasattr(report, 'eps'):
-                print(report.eps)
-            if hasattr(report, 'epc'):
-                print(report.epc)
-            if hasattr(report, 'epd'):
-                print(report.epd)
-            if hasattr(report, 'epu'):
-                print(report.epu)
-            if hasattr(report, 'epv'):
-                print(report.epv)
-            if hasattr(report, 'ept'):
-                print(report.ept)
-            if hasattr(report, 'eps'):
-                print(report.eps)
-            if hasattr(report, 'epc'):
-                print(report.epc)
-            if hasattr(report, 'epd'):
-                print(report.epd)
-            if hasattr(report, 'epu'):
-                print(report.epu)
-            if hasattr(report, 'epv'):
-                print(report.epv)
-            if hasattr(report, 'ept'):
-                print(report.ept)
-
-    except KeyError:
-        pass
+        # get data from gpsd
+        session.next()
+        # get data from serial port
+        data = ser.readline()
+        # print data
+        print(data)
+        # get position data
+        getPositionData()
     except KeyboardInterrupt:
-        quit()
+        print("\nExiting...")
+        sys.exit(0)
     except StopIteration:
-        session = None
         print("GPSD has terminated")
-    time.sleep(0.1)
-
-
-# close serial port
-ser.close()
-
-
-# close gpsd
-session = None
-print("GPSD has terminated")
-
-
-# end of file
-sys.exit(0)
-
-
+        sys.exit(0)
+    except Exception:
+        print("Error:", sys.exc_info()[0])
+        sys.exit(0)
+    time.sleep(1)
